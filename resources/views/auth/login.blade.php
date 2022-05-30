9b631c551a3b5baee3e9e490bb43fc825a37c725
Ng Jia Yong <46757018+callmeyonggor@users.noreply.github.com>
2022-05-30 14:34:31 +0800

    remove asset from css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<link href="/css/app.css" rel="stylesheet">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
