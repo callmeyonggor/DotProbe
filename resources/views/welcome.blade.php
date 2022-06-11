6887c0cc24d75ff0ae95e5f6d1cc1c350e5ce1d9
Ng Jia Yong <46757018+callmeyonggor@users.noreply.github.com>
2022-06-11 22:59:48 +0800

    added role
    -tested with both role: user and admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,21 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><b class="fs-3">{{ __('Instruction') }}</b></div>
+
+                <div class="card-body p-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<button type="button" class="btn btn-danger btn-lg position-absolute bottom-0 start-50 translate-middle-x w-50">Start Test</button>
+@endsection
