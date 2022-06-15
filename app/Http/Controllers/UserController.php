2fd7f3aebeb656e8d04468d29f23036121a03486
Ng Jia Yong <46757018+callmeyonggor@users.noreply.github.com>
2022-06-15 23:00:21 +0800

    added user filter
    -filter by: freetext, role and orderby

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,37 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function user_listing(){
-        $user = User::paginate(15);
-        return view('user/listing',['user' => $user]);
+    public function user_listing(Request $request){
+        $search = array();
+        if ($request->isMethod('post')) {
+            $submit_type = $request->input('submit');
+            switch ($submit_type) {
+                case 'search':
+                    session(['user_search' => [
+                        "freetext" =>  $request->input('freetext'),
+                        "role" =>  $request->input('role'),
+                        "sort" =>  $request->input('sort'),
+                    ]]);
+                    break;
+                case 'reset':
+                    session()->forget('user_search');
+                    break;
+            }
+        }
+        $search = session('user_search') ? session('user_search') : $search;
+        return view('user/listing',[
+            'submit' =>  route('user_listing'),
+            'search' =>  $search,
+            'user' => User::get_record($search, 10),
+            'role_sel' => [
+                ' ' => 'Please Select Role',
+                '1' => 'User',
+                '2' => 'Admin'
+            ],
+            'sort_sel' => [
+                '1' => 'Ascending',
+                '2' => 'Descending'
+            ],
+        ]);
     }
 }
