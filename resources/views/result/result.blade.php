474256f7c2e30285f287ccb6e26836ffad34182a
Ng Jia Yong <46757018+callmeyonggor@users.noreply.github.com>
2022-07-23 15:57:09 +0800

    change back loop to 120

--- resources/views/result/result.blade.php
+++ resources/views/result/result.blade.php
@@ -1,13 +1,15 @@
 <body>
-    <a href="/">
-        <h1><i class="fa-solid fa-angle-left"></i> Go Back</h1>
-    </a>
-    <div class="position-absolute top-50 start-50 translate-middle">
-        <h1>Total_correct: {{$total_correct}}</h1>
-        <h1>Average Response Time: {{$avg_response}}ms</h1>
-        <h1>Total Incongruent: {{$total_incongruents}}</h1>
-        <h1>Average Incongruent Response Time: {{$avg_incongruent_response}}ms</h1>
-        <h1>Total Congruent: {{$total_congruents}}</h1>
-        <h1>Average Congruent Response Time: {{$avg_congruent_response}}ms</h1>
+    <div class="container-fluid">
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <a href="/">
+                <p class="fs-1"><i class="fa-solid fa-angle-left"></i> Go Back</p>
+            </a>
+            <p class="fs-3">Total_correct: {{$total_correct}}
+                Average Response Time: {{$avg_response}}ms<br>
+                Total Incongruent: {{$total_incongruents}}<br>
+                Average Incongruent Response Time: {{$avg_incongruent_response}}ms<br>
+                Total Congruent: {{$total_congruents}}<br>
+                Average Congruent Response Time: {{$avg_congruent_response}}ms<br>
+        </div>
     </div>
 </body>
