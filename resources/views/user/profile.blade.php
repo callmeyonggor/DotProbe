7cd93b236274e950e0d7097cc06f897aab1a5667
Ng Jia Yong <46757018+callmeyonggor@users.noreply.github.com>
2022-09-11 12:36:50 +0800

    Update profile detail design
    three column per row to two column per row

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -17,7 +17,14 @@
     @endif
     <form method="POST" action="/profile/{{ $user->id }}">
         @csrf
-        <div class="card-header">Account Details</div>
+        <div class="card-header">Account Details
+            <div class="float-end">
+                <div class="form-check form-switch">
+                    <label>Is Admin?</label>
+                    <input class="form-check-input" type="checkbox" id="switch" name="switch" {{ $user->is_admin == 1 ? 'checked' : '' }} disabled>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col">
                 <div class="form-group">
@@ -25,52 +32,54 @@
                     <input type="text" class="form-control" name="name" placeholder="Name" value="{{ $user->name }}" required>
                 </div>
                 <div class="form-group">
-                    <label>Email</label>
-                    <input type="text" class="form-control" value="{{ $user->email }}" readonly>
-                </div>
-                <div class="form-group">
                     <label>Course of study/Job title</label>
                     <input type="text" class="form-control" name="job" placeholder="Course of study/Job title" value="{{ (isset($post->job) ? $post->job : $user->job) }}" required>
-                </div>
-            </div>
-            <div class="col">
-                <div class="form-group">
-                    <label>Age</label>
-                    <input type="number" class="form-control" name="age" placeholder="Age" value="{{ (isset($post->age) ? $post->age : $user->age) }}" required>
                 </div>
                 <div class="form-group">
                     <label>Height (in CM)</label>
                     <input type="number" step="any" class="form-control" name="height" placeholder="Height" value="{{ (isset($post->height) ? $post->height : $user->height) }}" required>
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" class="form-control" value="{{ $user->email }}" readonly>
+                </div>
+                <div class="form-group">
+                    <label>Age</label>
+                    <input type="number" class="form-control" name="age" placeholder="Age" value="{{ (isset($post->age) ? $post->age : $user->age) }}" required>
                 </div>
                 <div class="form-group">
                     <label>Weight (in KG)</label>
                     <input type="number" step="any" class="form-control" name="weight" placeholder="Weight" value="{{ (isset($post->weight) ? $post->weight : $user->weight) }}" required>
                 </div>
             </div>
+        </div>
+        <div class="row">
             <div class="col">
                 <div class="form-group">
                     <label>Race</label>
                     {!! Form::select('race', $race_sel, (isset($post->race) ? $post->race : $user->race), ['class' => 'form-control select_active']) !!}
                 </div>
+            </div>
+            <div class="col">
                 <div class="form-group">
                     <label>Education Level</label>
                     {!! Form::select('education', $education_sel, (isset($post->education) ? $post->education : $user->education), ['class' => 'form-control select_active']) !!}
                 </div>
+            </div>
+            <div class="col">
                 <div class="form-group">
                     <label>Handedness</label>
                     {!! Form::select('handedness', $handedness_sel, (isset($post->handedness) ? $post->handedness : $user->handedness), ['class' => 'form-control select_active']) !!}
-                </div>
-                <div class="float-end">
-                    <div class="form-check form-switch">
-                        <label>Is Admin?</label>
-                        <input class="form-check-input" type="checkbox" id="switch" name="switch" {{ $user->is_admin == 1 ? 'checked' : '' }} disabled>
-                    </div>
                 </div>
             </div>
         </div>
         <button type="submit" class="btn btn-primary float-end" name="submit" value="update_profile">Update Profile</button>
         <button type="submit" class="btn btn-light float-end" name="submit" value="cancel">Cancel</button>
     </form>
+</div>
+
 </div>
 </div>
 
